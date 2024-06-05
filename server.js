b2b9f90ac8790f7ede26ce96fac5b0c087ad294c
Patrick Breitenbach <pbreitenbach@gmail.com>
2024-06-04 22:23:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.post('/create-checkout-session', async (req, res) => {
     line_items: [
       {
         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: '{{PRICE_ID}}',
+        price: 'price_0OWV1Dy7kuAfVrQhZFlMa1T8',
         quantity: 1,
       },
     ],
